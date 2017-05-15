5fc6d366dfe0d0d921f7c3210d0e1787ff18466b
Andrés Urrego <andres.urrego@rappi.com>
2017-05-14 20:41:34 -0500

    fix comments

--- app/src/main/java/sharecar/dev/com/sharecar/ui/bindings/GeneralBinding.java
+++ app/src/main/java/sharecar/dev/com/sharecar/ui/bindings/GeneralBinding.java
@@ -1,9 +1,7 @@
 package sharecar.dev.com.sharecar.ui.bindings;
 
 import android.databinding.BindingAdapter;
-import android.support.annotation.StringRes;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -12,11 +10,6 @@ public class GeneralBinding {
     @BindingAdapter("load_image")
     public static void loadImage(ImageView imageView, String urlPhoto) {
         Glide.with(imageView.getContext()).load(urlPhoto).into(imageView);
-    }
-
-    @BindingAdapter({"format", "text"})
-    public static void setTextWithFormat(TextView textView, @StringRes int resource, String text) {
-        textView.setText(textView.getContext().getString(resource, text));
     }
 
 }
